aecd936181490625e57fc8938077c886f5d697d1
Lucas-Lima <lucasphenrique@hotmail.com>
2025-03-07 13:50:53 +0100

    Adição de representação de conectividade e quantidade de documentos para o grafo de blocos com SBM

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import spacy
+import math
 import pandas as pd
 import numpy as np
 from graph_tool.all import *
@@ -17,13 +18,15 @@ def initialize_graph():
 
     # Definir propriedades
     name_prop = g.new_vertex_property("string")
-    type_prop = g.new_vertex_property("string")
+    type_prop = g.new_vertex_property("int")
+    qtd_prop = g.new_vertex_property("int")
     edge_weight = g.new_edge_property("int")
     color_prop = g.new_vertex_property("vector<double>")
     size_prop = g.new_vertex_property("double")
     label_prop = g.new_vertex_property("string")
     g.vp["color"] = color_prop
     g.vp["size"] = size_prop
+    g.vp["amount"] = qtd_prop
     g.vp["label"] = label_prop
     g.vp["name"] = name_prop
     g.vp["tipo"] = type_prop
@@ -32,14 +35,13 @@ def initialize_graph():
     return g
 
 def build_block_graph(block_graph, state, g):
-
     # Visualizo o grafo de blocos antes das tratativas
     visualize_graph_bl(block_graph, "outputs/text_block_graph_original.pdf")
     
     # Definir propriedades block graph
     name_prop = block_graph.new_vertex_property("string")
-    type_prop = block_graph.new_vertex_property("string")
-    edge_weight = block_graph.new_edge_property("int")
+    type_prop = block_graph.new_vertex_property("int")
+    edge_weight = block_graph.new_edge_property("double")
     number_vertex = block_graph.new_vertex_property("int")
     color_prop = block_graph.new_vertex_property("vector<double>")
     size_prop = block_graph.new_vertex_property("double")
@@ -61,27 +63,57 @@ def build_block_graph(block_graph, state, g):
 
     # Criar um dicionário que associa cada bloco do grafo de blocos aos seus vértices no grafo original
     block_to_vertices = {}
-
+    
     for i in range(len(block_sizes)):  # Iterar sobre os índices dos blocos
         block_vertices = [v for v in range(len(state.get_blocks().a)) if state.get_blocks().a[v] == i]  
         block_to_vertices[i] = block_vertices  # Salvar no dicionário
 
-        terms = [g.vp["name"][g.vertex(v)] for v in block_vertices if g.vp["tipo"][g.vertex(v)] == "Term"]
-        docs = sum(1 for v in block_vertices if g.vp["tipo"][g.vertex(v)] == "Document")
+        terms = [
+            (g.vp["name"][g.vertex(v)], g.vp['amount'][g.vertex(v)]) 
+            for v in block_vertices if g.vp["tipo"][g.vertex(v)] == 1
+        ]
+        docs = sum(1 for v in block_vertices if g.vp["tipo"][g.vertex(v)] == 0)
 
         if terms and docs:
-            block_graph.vp["tipo"][i] = "Ambos"
+            block_graph.vp["tipo"][i] = 11  # Ambos
         elif terms:
-            block_graph.vp["tipo"][i] = "Term"
+            block_graph.vp["tipo"][i] = 1  # Termos
         elif docs:
-            block_graph.vp["tipo"][i] = "Document"
+            block_graph.vp["tipo"][i] = 0  # Documentos
         else:
-            block_graph.vp["tipo"][i] = "Desconhecido"
+            block_graph.vp["tipo"][i] = 22  # Desconhecido
 
-        # Atualiza a name do bloco para mostrar alguns termos (caso seja um bloco de termos)
+        # Atualiza o nome do bloco para mostrar alguns termos (caso seja um bloco de termos)
         if terms:
-            block_graph.vp["name"][i] = ", ".join(terms[:3])  # Mostra até 3 termos na label
+            sorted_terms = sorted(terms, key=lambda x: x[1], reverse=True)
+            block_graph.vp["name"][i] = "|".join([name for name, _ in sorted_terms[:3]])  # Pega os 3 primeiros nomes
+            # print(block_graph.vp["name"][i])
 
+
+    # Calcular a força das conexões entre blocos
+    block_connections = {}  # Dicionário para armazenar a força das conexões entre blocos
+    for edge in g.edges():
+        source_block = blocks[int(edge.source())]
+        target_block = blocks[int(edge.target())]
+        if source_block != target_block:  # Ignora arestas dentro do mesmo bloco
+            key = tuple(sorted((source_block, target_block)))  # Chave única para o par de blocos
+            if key in block_connections:
+                block_connections[key] += g.ep["weight"][edge]  # Soma o peso da aresta
+            else:
+                block_connections[key] = g.ep["weight"][edge]  # Inicializa o peso da aresta
+
+    # Definir a largura das arestas com base na força das conexões
+    for edge in block_graph.edges():
+        source_block = int(edge.source())
+        target_block = int(edge.target())
+        key = tuple(sorted((source_block, target_block)))
+
+        if key in block_connections:
+            block_graph.ep["weight"][edge] = block_connections[key]  # Define o peso
+
+        # Mapeamento para a largura das arestas (normalização para melhor visualização)
+        # print(block_graph.ep["weight"][edge], max(1.0, block_graph.ep["weight"][edge] / 75), max(1.0, (block_graph.ep["weight"][edge] ** 0.5) / 2), max(1.0, math.log1p(block_graph.ep["weight"][edge])))
+        block_graph.ep["weight"][edge] = max(1.0, block_graph.ep["weight"][edge] / 75) # Ajuste conforme necessário
 
     # Remove vértices vazios do grafo de blocos
     to_remove = [v for v in block_graph.vertices() if v.out_degree() == 0 and v.in_degree() == 0]
@@ -91,31 +123,32 @@ def build_block_graph(block_graph, state, g):
 
     vertices = list(block_graph.vertices())
     for v in tqdm(vertices, desc="Building Block Graph SBM", total=len(vertices)):
-        if block_graph.vp["tipo"][v] == "Document":
+        if block_graph.vp["tipo"][v] == 0:  # Documento
             block_graph.vp["color"][v] = [1.0, 0.0, 0.0, 1.0]  # Vermelho (RGBA)
-            block_graph.vp["size"][v] = 20  # Tamanho maior para documentos
-            # block_graph.vp["shape"][v] = "circle"  # Termo: Quadrado ou Retângulo
-        else:
+            block_graph.vp["size"][v] = max(20, v.out_degree() * 10)
+        else:  # Termo
             block_graph.vp["color"][v] = [0.0, 0.0, 1.0, 1.0]  # Azul (RGBA)
-            # block_graph.vp["size"][v] = 10  # Tamanho menor para termos
-            # block_graph.vp["shape"][v] = "square"  # Termo: Quadrado ou Retângulo
+            block_graph.vp["size"][v] = 10  # Tamanho menor para termos
 
     # Aplicação do SBM ao grafo de blocos
     state_bg = minimize_blockmodel_dl(block_graph)
     pos = sfdp_layout(block_graph)
+
+    # Agora usamos a propriedade edge_pen_width na visualização
     state_bg.draw(
         pos=pos,
-        vertex_fill_color=block_graph.vp["color"],   # Define a cor dos vértices
-        vertex_size=block_graph.vp["size"],          # Define o tamanho dos vértices
-        vertex_text=block_graph.vp["name"],         # Exibe os rótulos dos vértices
-        vertex_font_size=10,             # Tamanho da fonte dos rótulos
-        # vertex_shape=block_graph.vp["shape"],        # Define o formato do vértice
-        output_size=(800, 800),         # Tamanho da saída
-        output="outputs/text_block_graph_sbm.pdf"     # Arquivo PDF de saída
+        edge_pen_width=block_graph.ep["weight"],  # Usa os pesos calculados para definir a largura das arestas
+        vertex_fill_color=block_graph.vp["color"],  # Define a cor dos vértices
+        vertex_size=block_graph.vp["size"],  # Define o tamanho dos vértices
+        vertex_text=block_graph.vp["name"],  # Exibe os rótulos dos vértices
+        vertex_text_position = -2,
+        vertex_text_color = 'black',
+        vertex_font_size=10,  # Tamanho da fonte dos rótulos
+        output_size=(800, 800),  # Tamanho da saída
+        output="outputs/text_block_graph_sbm.pdf"  # Arquivo PDF de saída
     )
+
     return block_to_vertices
-
-
 
 def build_graph(g, df, nlp):
     map_termos = {}
@@ -123,8 +156,9 @@ def build_graph(g, df, nlp):
     for index,row in tqdm(df.iterrows(),desc="DF Interation", total=len(df)):
         v1 = g.add_vertex()
         g.vp["name"][v1] = row["title"]
-        g.vp["tipo"][v1] = "Document"
+        g.vp["tipo"][v1] = 0 #Documentos
         map_termos[row["title"]] = v1
+        g.vp['amount'][v1] = 1
 
         doc = nlp(row["abstract"])
 
@@ -133,11 +167,13 @@ def build_graph(g, df, nlp):
             if not termo.is_stop and not termo.is_punct:
                 # Verificar se o termo já existe no grafo
                 if termo.text in map_termos:
+                    g.vp['amount'][v2] += 1
                     v2 = map_termos[termo.text]
                 else:
-                    v2 = g.add_vertex()                
+                    v2 = g.add_vertex()
+                    g.vp['amount'][v2] = 1        
                     g.vp['name'][v2] = termo.text
-                    g.vp["tipo"][v2] = "Term"
+                    g.vp["tipo"][v2] = 1 #Termos
                     map_termos[termo.text] = v2
 
                 # Verificar se existe uma aresta entre os vértices v1 e v2
@@ -156,7 +192,7 @@ def visualize_graph(g, graph_name):
     vertices = list(g.vertices())
 
     for v in tqdm(vertices, desc="Building Graph", total=len(vertices)):
-        if g.vp["tipo"][v] == "Document":
+        if g.vp["tipo"][v] == 0:
             g.vp["color"][v] = [1.0, 0.0, 0.0, 1.0]  # Vermelho (RGBA)
             g.vp["size"][v] = 20  # Tamanho maior para documentos
         else:
@@ -190,27 +226,22 @@ def visualize_graph_bl(g, graph_name):
 
 
 def min_sbm_wew(g):
-    # # Se não for passado um argumento, mas o grafo tem pesos, definir state_args automaticamente
-    # if state_args is None and "weight" in g.ep:
-    #     state_args = {"eweight": g.ep["weight"]}
-    
-    # #Inferindo comunidades usando o SBM de maneira mais simples possível
-    # state = minimize_blockmodel_dl(g, **(state_args or {}))
-    state = minimize_blockmodel_dl(g, state_args={"eweight": g.ep["weight"]})
 
+     # #Inferindo comunidades usando o SBM de maneira mais simples possível
+    state = minimize_blockmodel_dl(g, state_args={"eweight": g.ep["weight"], "pclabel": g.vp["tipo"]})
 
     # Desenhar as comunidades inferidas com as per'sonalizações
     state.draw(
+        pos = sfdp_layout(g),
         vertex_fill_color=g.vp["color"],   # Define a cor dos vértices
         vertex_size=g.vp["size"],          # Define o tamanho dos vértices
         vertex_text=g.vp["label"],         # Define o rótulo dos vértices (ID)
         vertex_font_size=8,             # Tamanho da fonte dos rótulos
         output_size=(800, 800),         # Tamanho da saída
-        output="outputs/text_graph_sbm.pdf"     # Arquivo PDF de saída
+        output="outputs/text_graph_sbm.pdf"    # Arquivo PDF de saída
     )
 
     return state
-
 
 def edge_matrix(state, fig_name, g):
     # Reorganizar os nós para garantir que estejam em ordem contígua
@@ -234,8 +265,8 @@ def edge_matrix(state, fig_name, g):
     for i, size in enumerate(block_sizes):
         # Identificar quais tipos de vértices estão presentes no bloco
         block_vertices = [v for v in range(len(state.get_blocks().a)) if state.get_blocks().a[v] == i] # Cria uma lista com os índices dos vértices que pertencem ao bloco de índice 'i'
-        terms = sum(1 for v in block_vertices if g.vp["tipo"][g.vertex(v)] == "Term")
-        docs = sum(1 for v in block_vertices if g.vp["tipo"][g.vertex(v)] == "Document")
+        terms = sum(1 for v in block_vertices if g.vp["tipo"][g.vertex(v)] == 1)
+        docs = sum(1 for v in block_vertices if g.vp["tipo"][g.vertex(v)] == 0)
         
         if terms > 0 and docs > 0:
             group_type = "Ambos"
@@ -248,41 +279,12 @@ def edge_matrix(state, fig_name, g):
 
         print(f"O grupo {i} possui {size} vértices e é classificado como {group_type}.")
 
-
-def nested_sbm(g):
-    # Nested SBM
-    state = minimize_nested_blockmodel_dl(g)
-    state.draw(bg_color='white',
-        output="outputs/text-nsbm-fit.svg"
-        )
-
-    state.print_summary()
-
-    # Obter os níveis hierárquicos do SBM
-    levels = state.get_levels()  # Retorna uma lista de estados, cada um representando um nível hierárquico
-    for s in levels:
-        print(s)  # Exibe informações detalhadas sobre o nível atual
-        if s.get_N() == 1:  # Se o nível tiver apenas 1 bloco, a hierarquia chegou ao nível mais alto
-            break  # Interrompe a iteração, pois não há mais subdivisões a explorar
-
-    # Nível 0: Bloco ao qual o nó 33 pertence na partição mais detalhada
-    r = levels[0].get_blocks()[33]  # `get_blocks()` retorna a atribuição de blocos para cada nó no nível 0
-    print(f"\n\nNível 0: O nó 33 pertence ao bloco {r}")
-
-    # Nível 1: Bloco ao qual o bloco de nível 0 foi agrupado
-    r = levels[1].get_blocks()[r]  # Atribuição do bloco de nível superior para o bloco do nível 0
-    print(f"Nível 1: O bloco anterior foi agrupado no bloco {r}")
-
-    # Nível 2: Bloco ao qual o bloco de nível 1 foi agrupado
-    r = levels[2].get_blocks()[r]  # Atribuição do bloco de nível superior para o bloco do nível 1
-    print(f"Nível 2: O bloco anterior foi agrupado no bloco {r}\n")
-
-    return state
-
 def nested_sbm_wew(g, output_name):
     # Nested SBM
     state = minimize_nested_blockmodel_dl(g, state_args={"eweight": g.ep["weight"]})
-    state.draw(bg_color='white',
+    state.draw(
+        pos = sfdp_layout(g),
+        bg_color='white',
         output=output_name
         )
 
@@ -345,13 +347,14 @@ def main():
     df = pd.read_parquet("wos_sts_journals.parquet")
     g = initialize_graph()
     # Selecionar uma amostra do DataFrame    
-    df = df.sample(n=30, random_state=42)
+    df = df.sample(n=300, random_state=42)
     #Construção do grafo
     g = build_graph(g,df,nlp)
     print(g)
     visualize_graph(g, "outputs/text_graph.pdf")
     #Aplicação do sbm com a propriedade de peso nas arestas
     state_wew = min_sbm_wew(g)
+
     print(f"O grafo original possui {state_wew.get_B()} blocos após o SBM, sendo que {state_wew.get_nonempty_B()} não estão vazios.")
     #Construção do grafo de blocos
 
@@ -359,19 +362,21 @@ def main():
 
     build_block_graph(block_graph, state_wew, g)
 
-    #Matriz de arestas entre os blocos do grafo original
-    edge_matrix(state_wew, "outputs/text-edge-counts.svg", g)
-    #Aplicação do Nested SBM para o grafo original
-    state_nested_wew = nested_sbm_wew(g, "outputs/text-edge-counts-nsbm.svg")
-    #Aplicação do Nested SBM para o grafo de blocos
-    state_nested_block_graph = minimize_nested_blockmodel_dl(block_graph)
-    state_nested_block_graph.draw(bg_color='white',
-        output="outputs/text-edge-counts-nsbm-block_graph.svg"
-        )
+    # #Matriz de arestas entre os blocos do grafo original
+    # edge_matrix(state_wew, "outputs/text-edge-counts.svg", g)
+    # #Aplicação do Nested SBM para o grafo original
+    # state_nested_wew = nested_sbm_wew(g, "outputs/text-edge-counts-nsbm.svg")
+    # #Aplicação do Nested SBM para o grafo de blocos
+    # state_nested_block_graph = minimize_nested_blockmodel_dl(block_graph)
+    # state_nested_block_graph.draw(
+    #     pos = sfdp_layout(block_graph),
+    #     bg_color='white',
+    #     output="outputs/text-edge-counts-nsbm-block_graph.svg"
+    #     )
     #Refinamento do state do Nested SBM
     # refine_mcmc(state_nested_wew, g)
 
     print(f"\nO tempo total de execução desse código foi de :{time.time() - start_time:.2f} segundos")
 
-if __name__ == "__main__":
-    main()
+# if __name__ == "__main__":
+#     main()
